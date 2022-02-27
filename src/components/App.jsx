62787f8ee01e4f7df18516a5062bfb23b11b1f3f
Carlos Ortiz <carlospianiforte@gmail.com>
2022-02-27 03:53:56 -0500

    comit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import '../styles/components/App.scss';
 
 const App = () => {
   return (
-    <div><h1>HOLA TOCAS</h1></div>
-  )
-}
+    <div>
+      <h1>HOLA TOCAS</h1>
+    </div>
+  );
+};
 
-export default App
+export default App;
